553800af786673b09b7679cea2b3a6593e72a1b3
Mpadhi2000 <mayankpadhi91@gmail.com>
2023-04-05 13:36:42 +0530

    call.js modified

--- Week-3/call.js
+++ Week-3/call.js
@@ -2,10 +2,11 @@ let userDetails = {
   name : "mayank padhi",
   age: 23,
   designation: "student",
-}
+};
+
 let printDetails = function(state,country) {
   console.log(this.name+" "+state+" "+country);
-}
+};
 
 printDetails.call(userDetails,"Delhi","India");
 
@@ -13,13 +14,14 @@ let userDetails2 = {
   name : "chetna padhi",
   age: 18,
   designation: "student",
-}
+};
+
 // call method
 printDetails.call(userDetails2,"mumbai","india");
 
-//Apply method
+// Apply method
 printDetails.apply(userDetails2,["mumbai","india"]);
 
-//bind Method
-let newfun = printDetails.call(userDetails2,"mumbai","India");
+// Bind method
+let newfun = printDetails.bind(userDetails2, "mumbai", "India");
 newfun();
